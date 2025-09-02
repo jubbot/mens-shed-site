360d278b72f1222f6ea4dbe2a7b9251a14e51a51
William Thomas <wilbourne.creatives@gmail.com>
2025-09-02 16:09:35 +1000

    Removed Members

--- src/lib/supabaseAdmin.ts
+++
@@ -1,8 +0,0 @@
-// src/lib/supabaseAdmin.ts
-import { createClient } from '@supabase/supabase-js';
-
-export const supabaseAdmin = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!, // never expose to browser
-  { auth: { autoRefreshToken: false, persistSession: false } }
-);
